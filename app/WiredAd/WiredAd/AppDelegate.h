69ebef88312355bae0048c092757c11aeb5b730f
Mike Newell <mike_newell@gspsf.com>
2015-07-29 10:53:23 -0700

    init

---
+++ app/WiredAd/WiredAd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WiredAd
+//
+//  Created by Mike Newell on 7/29/15.
+//  Copyright (c) 2015 Mike Newell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ app/WiredAd/WiredAd/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WiredAd
+//
+//  Created by Mike Newell on 7/29/15.
+//  Copyright (c) 2015 Mike Newell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
